fc413526c9d3b586c8358b2bc9ede25e0ad3410e
verbitsky.vladislav <vladislav.verbitsky.bis@gmail.com>
2025-06-25 23:46:17 +0400

    fix config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    basePath: "",
     reactStrictMode: true,
     output: 'export',
     distDir: 'dist',
